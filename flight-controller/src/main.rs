2d554cfcaf5bb8f0dccbe1e67e0b51967d3ac0cc
Graicc <33105645+Graicc@users.noreply.github.com>
2025-04-22 15:19:38 -0400

    Send PWM signal to hover

--- flight-controller/src/main.rs
+++ flight-controller/src/main.rs
@@ -3,7 +3,10 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    time::hz,
+};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -13,13 +16,34 @@ async fn main(_spawner: Spawner) {
     info!("Hello World!");
 
     let mut led = Output::new(p.PB5, Level::High, Speed::Low);
+    // let mut led2 = Output::new(p.PB0, Level::High, Speed::Low);
+
+    let pwm_pin = embassy_stm32::timer::simple_pwm::PwmPin::new_ch3(
+        p.PB0,
+        embassy_stm32::gpio::OutputType::PushPull,
+    );
+    let mut pwm = embassy_stm32::timer::simple_pwm::SimplePwm::new(
+        p.TIM3,
+        None,
+        None,
+        Some(pwm_pin),
+        None,
+        // hz(16_000),
+        hz(480),
+        embassy_stm32::timer::low_level::CountingMode::EdgeAlignedUp,
+    );
+    let mut motor = pwm.ch3();
+    motor.enable();
 
     loop {
-        info!("high");
         led.set_high();
-        Timer::after_secs(2).await;
+        // off
+        motor.set_duty_cycle_fraction(1000, 2083);
+        Timer::after_secs(5).await;
 
-        info!("low");
+        // on
+        // motor.set_duty_cycle_fraction(1337, 2083);
+        motor.set_duty_cycle_fraction(1000, 2083);
         led.set_low();
         Timer::after_secs(2).await;
     }
