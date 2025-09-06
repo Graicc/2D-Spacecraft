56ab29cbc006f1d42091aeda5ff3f4ba740ee5e2
Graicc <33105645+Graicc@users.noreply.github.com>
2025-09-05 21:53:40 -0400

    Get logging working on flight controller

--- flight-controller/src/main.rs
+++ flight-controller/src/main.rs
@@ -1,50 +1,127 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use core::panic::PanicInfo;
+
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
 use embassy_stm32::{
+    Peri,
     gpio::{Level, Output, Speed},
-    time::hz,
+    peripherals::{self, PA11, PA12, USB_OTG_FS},
+    time::Hertz,
+    usb::{self, Driver},
 };
-use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
+use embassy_time::{Duration, Timer};
+use embassy_usb::{
+    Builder,
+    class::cdc_acm::{CdcAcmClass, State},
+};
+
+embassy_stm32::bind_interrupts!(struct Irqs {
+    OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
+});
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    log::error!("{info}"); // TODO: this doesn't work
+    cortex_m::asm::udf();
+}
+
+#[embassy_executor::task]
+async fn logger_task(
+    peri: Peri<'static, USB_OTG_FS>,
+    dp: Peri<'static, PA12>,
+    dm: Peri<'static, PA11>,
+) {
+    // Create the driver, from the HAL.
+    let mut ep_out_buffer = [0u8; 256];
+    let mut config = embassy_stm32::usb::Config::default();
+
+    // Do not enable vbus_detection. This is a safe default that works in all boards.
+    // However, if your USB device is self-powered (can stay powered on if USB is unplugged), you need
+    // to enable vbus_detection to comply with the USB spec. If you enable it, the board
+    // has to support it or USB won't work at all. See docs on `vbus_detection` for details.
+    config.vbus_detection = false;
+
+    let driver = Driver::new_fs(peri, Irqs, dp, dm, &mut ep_out_buffer, config);
+
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut control_buf,
+    );
+
+    // Create classes on the builder.
+    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // This has to be before usb fut
+    let log_fut = embassy_usb_logger::with_class!(1024, log::LevelFilter::Debug, class);
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    join(usb_fut, log_fut).await;
+}
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+async fn main(spawner: Spawner) -> ! {
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(8_000_000),
+            // mode: HseMode::Bypass,
+            mode: HseMode::Oscillator, // We want to use the internal oscillator! We don't have an external one
+        });
+        config.rcc.pll_src = PllSource::HSE;
+        config.rcc.pll = Some(Pll {
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL168,
+            divp: Some(PllPDiv::DIV2), // 8mhz / 4 * 168 / 2 = 168Mhz.
+            divq: Some(PllQDiv::DIV7), // 8mhz / 4 * 168 / 7 = 48Mhz.
+            divr: None,
+        });
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV4;
+        config.rcc.apb2_pre = APBPrescaler::DIV2;
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q;
+    }
+    let p = embassy_stm32::init(config);
 
-    let mut led = Output::new(p.PB5, Level::High, Speed::Low);
-    // let mut led2 = Output::new(p.PB0, Level::High, Speed::Low);
+    spawner
+        .spawn(logger_task(p.USB_OTG_FS, p.PA12, p.PA11))
+        .unwrap();
 
-    let pwm_pin = embassy_stm32::timer::simple_pwm::PwmPin::new_ch3(
-        p.PB0,
-        embassy_stm32::gpio::OutputType::PushPull,
-    );
-    let mut pwm = embassy_stm32::timer::simple_pwm::SimplePwm::new(
-        p.TIM3,
-        None,
-        None,
-        Some(pwm_pin),
-        None,
-        // hz(16_000),
-        hz(480),
-        embassy_stm32::timer::low_level::CountingMode::EdgeAlignedUp,
-    );
-    let mut motor = pwm.ch3();
-    motor.enable();
+    // Onboard led
+    let mut led: Output<'_> = Output::new(p.PB5, Level::High, Speed::Low);
 
     loop {
         led.set_high();
-        // off
-        motor.set_duty_cycle_fraction(1000, 2083);
-        Timer::after_secs(5).await;
-
-        // on
-        // motor.set_duty_cycle_fraction(1337, 2083);
-        motor.set_duty_cycle_fraction(1000, 2083);
+        log::info!("Blink");
+        Timer::after(Duration::from_millis(100)).await;
         led.set_low();
-        Timer::after_secs(2).await;
+        Timer::after(Duration::from_millis(100)).await;
     }
 }
