134ffa670781b4c333544471acdf731ed63abcc2
Graicc <33105645+Graicc@users.noreply.github.com>
2026-01-02 13:29:29 -0500

    Add camera calibration

--- tracking/camera.py
+++ tracking/camera.py
@@ -6,11 +6,35 @@ import numpy as np
 import cv2
 
 
+# Constants from calibration routine
+mtx = np.array(
+    [
+        [711.57891517, 0.0, 480.45785036],
+        [0.0, 712.53778636, 303.15145412],
+        [0.0, 0.0, 1.0],
+    ]
+)
+
+dist = np.array(
+    [[-2.53347121e-01, 1.15335368e-02, 1.12339388e-04, 8.78180118e-04, 2.79723320e-01]]
+)
+
+newcameramtx = np.array(
+    [
+        [648.00105646, 0.0, 481.45773943],
+        [0.0, 648.51300997, 303.400218],
+        [0.0, 0.0, 1.0],
+    ]
+)
+
 class Tracker:
     __slots__ = ("camera", "latest_image", "latest_position", "blob_detector")
 
     def __init__(self):
-        self.camera = cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture(0, cv2.CAP_V4L2)
+        # self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        # self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.latest_image = None
         self.latest_position = None
         params = cv2.SimpleBlobDetector.Params()
@@ -22,6 +46,9 @@ class Tracker:
         ok, frame = self.camera.read()
         if not ok:
             return False
+
+
+        frame = cv2.undistort(frame, mtx, dist, None, newcameramtx)
 
         self.latest_image = frame
 
